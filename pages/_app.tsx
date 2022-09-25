18c6c8d3941cea26a80658d32f29ef527ba3e4bd
gutovskiy.ru <sergey.gutovsk@gmail.com>
2022-09-25 20:41:31 +0300

    added favicon and title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,26 @@ import type { AppProps } from 'next/app';
 
 import '@styles/reset.scss';
 import '@styles/root.scss';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Гутовский Сергей</title>
+        <meta name="description" content="Мой персональный блог." />        
+
+        <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/imagesfavicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png" />
+        <link rel="manifest" href="/images/site.webmanifest" />
+        <link rel="mask-icon" href="/images/safari-pinned-tab.svg" color="#000000" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
